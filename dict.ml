91a87e3b0e124a23198c08442781ddf14eabb402
David Chouinard <david@davidchouinard.com>
2013-03-15 18:27:44 -0400

    Implemented insert_downward

--- dict.ml
+++ dict.ml
@@ -470,15 +470,16 @@ struct
    * with the appropriate arguments. *)
   let rec insert_downward (d: dict) (k: key) (v: value) : kicked =
     match d with
-      | Leaf -> raise TODO (* base case! see handout *)
-      | Two(left,n,right) -> raise TODO (* mutual recursion *)
-      | Three(left,n1,middle,n2,right) -> raise TODO (* mutual recursion *)
+      | Leaf -> Up (Leaf, (k,v), Leaf)
+      | Two(left,n,right) -> insert_downward_two (k, v) n left right
+      | Three(left,n1,middle,n2,right) ->
+          insert_downward_three (k, v) n1 n2 left middle right
 
   (* Downward phase on a Two node. (k,v) is the (key,value) we are inserting,
    * (k1,v1) is the (key,value) of the current Two node, and left and right
    * are the two subtrees of the current Two node. *)
-  and insert_downward_two ((k,v): pair) ((k1,v1): pair) 
-      (left: dict) (right: dict) : kicked = 
+  and insert_downward_two ((k,v): pair) ((k1,v1): pair)
+      (left: dict) (right: dict) : kicked =
     raise TODO
 
   (* Downward phase on a Three node. (k,v) is the (key,value) we are inserting,
