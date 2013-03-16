9660e0f8859fa9a5293d5754f38a36f35e0ec48e
Matthew J. Holman <mholman@cfa.harvard.edu>
2013-03-16 10:00:02 -0400

    Reverted insert_downward to earlier version.  Progress but not there yet.

--- dict.ml
+++ dict.ml
@@ -478,34 +478,15 @@ struct
     (* TODO: I thought this was right, but then I read this: https://piazza.com/class#spring2013/cs51/1271
      * Now I'm not sure this is the correct implementation *)
     match d with
-      | Leaf -> Up (Leaf, (k,v), Leaf)
+      | Leaf -> (
+	print "Leaf case of insert_downward";
+	Up (Leaf, (k,v), Leaf))
       | Two(left,(kn,vn),right) -> (
 	print "Two case of insert_downward";
-	match insert_downward_two (k, v) (kn, vn) left right with
-	| Up(l,(k1,v1),r) ->
-	  (match D.compare k1 kn with 
-	  | Less | Eq -> Done (Three (l, (k1, v1), r, (kn, vn), right))
-	  | Greater -> Done (Three (left, (kn, vn), l, (k1, v1), r)))
-	| Done x -> Done x)
+	insert_downward_two (k, v) (kn, vn) left right)
       | Three(left,(kn1,vn1), middle,(kn2,vn2),right) -> (
 	print "Three case of insert_downward";
-        match insert_downward_three (k, v) (kn1, vn1) (kn2, vn2) left middle right with
-	| Up(l,(k1,v1),r) -> 
-	  (match D.compare k1 kn1, D.compare k1 kn2 with
-	  | (Less, Less) -> 
-	    Up ((Two (l, (k1, v1), r)),
-		(kn1, vn1),
-		(Two (middle, (kn2, vn2), right)))
-	  | (Greater, Less) -> 
-	    Up ((Two (left, (kn1, vn1), l)),
-		(k1, v1),
-		(Two (r, (kn2, vn2), right)))
-	  | (Greater, Greater) -> 
-	    Up ((Two (left, (kn1, vn1), middle)),
-		(kn2, vn2),
-		(Two (l, (k1, v1), r)))
- 	  | _ -> raise (Failure "Invariant error in insert_downward"))
-	| Done x -> Done x)
+        insert_downward_three (k, v) (kn1, vn1) (kn2, vn2) left middle right)
 	
   (* Downward phase on a Two node. (k,v) is the (key,value) we are inserting,
    * (k1,v1) is the (key,value) of the current Two node, and left and right
@@ -513,15 +494,25 @@ struct
   (* Should we raise an exception on the Eq case? *)
   and insert_downward_two ((k,v): pair) ((k1,v1): pair)
       (left: dict) (right: dict) : kicked =
-    match D.compare k k1 with
-    | Less | Eq -> insert_downward left k v 
-    | Greater -> insert_downward right k v
+    print "insert_downward_two";
+    let result = 
+      match D.compare k k1 with
+      | Less | Eq -> insert_downward left k v 
+      | Greater -> insert_downward right k v
+    in
+    match result with
+    | Up (l, (k', v'), r) ->
+      (match D.compare k' k1 with
+      | Less | Eq -> Done (Three (l, (k', v'), r, (k1, v1), right))
+      | Greater ->   Done (Three (left, (k1, v1), l, (k', v'), r)))
+    | Done x -> Done x
       
   (* Downward phase on a Three node. (k,v) is the (key,value) we are inserting,
    * (k1,v1) and (k2,v2) are the two (key,value) pairs in our Three node, and
    * left, middle, and right are the three subtrees of our current Three node *)
   and insert_downward_three ((k,v): pair) ((k1,v1): pair) ((k2,v2): pair) 
       (left: dict) (middle: dict) (right: dict) : kicked =
+    print "insert_downward_three";
     match (D.compare k k1), (D.compare k k2) with
     | (Less, Less) -> insert_downward left k v 
     | (Eq, Less) -> insert_downward left k v 
@@ -530,12 +521,11 @@ struct
     | (Greater, Greater) -> insert_downward right k v
     | _ -> raise (Failure "Invariant error in insert_downward_three")
 
-
   (* We insert (k,v) into our dict using insert_downward, which gives us
    * "kicked" up configuration. We return the tree contained in the "kicked"
    * configuration. *)
   let insert (d: dict) (k: key) (v: value) : dict =
-    print (string_of_tree d);
+    print ("insert: " ^ (string_of_tree d));
     match insert_downward d k v with
       | Up(l,(k1,v1),r) -> Two(l,(k1,v1),r)
       | Done x -> x
